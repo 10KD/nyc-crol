9cdb88a0dbb1dd46cd70454bca3b7c0628c52e26
10kd <mkim.don@gmail.com>
2018-05-30 15:15:42 -0700

    users api fixed

--- front-end/src/app/update-user/update-user.component.ts
+++ front-end/src/app/update-user/update-user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { USERS } from '../users';
+// import { USERS } from '../users';
+import { Http } from '@angular/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-update-user',
@@ -9,18 +11,35 @@ import { USERS } from '../users';
 })
 export class UpdateUserComponent implements OnInit {
   user: any;
-  // paramid: any;
-  // userName: String;
-  // firstName: String;
-  // lastName: String;
-  constructor(private route: ActivatedRoute) { }
+  paramid: any;
+  userName: String;
+  firstName: String;
+  lastName: String;
+  constructor(private route: ActivatedRoute, private http: Http) { }
+
+  updateUser(userName, firstName, lastName) {
+    this.http.patch(
+      `${environment.apiHost}/api/users/` + this.user.id,
+      {
+        "userName": userName,
+        "firstName": firstName,
+        "lastName": lastName
+      }
+    ).toPromise()
+      .then(response => console.log(response.json().userName + " has been updated."))
+  }
 
   ngOnInit() {
     this.route.params.forEach(param => {
       if (param.id) {
-        this.user = USERS[param.id - 1];
+        this.paramid = param.id;
       }
     });
-  }
+    this.http.get(`${environment.apiHost}/api/users/` + this.paramid)
+      .toPromise()
+      .then(response => {
+        this.user = response.json();
+        console.log(this.user);
+      }); 
 
 }
