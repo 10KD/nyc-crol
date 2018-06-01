22d3a89657ca3c1b822308d8e8e02fc605332bf6
10kd <mkim.don@gmail.com>
2018-06-01 04:12:34 -0700

    changed sql table

--- favorites-api/src/main/resources/db/migration/V1__Create_Favorites_Table.sql
+++ favorites-api/src/main/resources/db/migration/V1__Create_Favorites_Table.sql
@@ -1,6 +1,6 @@
 create table FAVORITES (
     ID serial,
-    FAVORITE_INFO varchar(100) NOT NULL,
+    FAVORITE_INFO varchar(200) NOT NULL,
     CATEGORY_ID varchar(100),
     USER_ID varchar(100)
 );
